05ecbe6bfc9594180a8bdb3a41c1fdd6dad17871
hamzamullan <80555783+hamzamullan@users.noreply.github.com>
2022-02-03 17:08:20 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ camera = document.getElementById("camera");
 
 Webcam.attach( '#camera' );
 
-function take_snapshot()
-
+function take_snapshot(){
 Webcam.snap(function(data_uri){
     document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
 });
 console.log('ml5 verion:',ml5.version)
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Cvad8iR31/',modelLoaded)
+}
